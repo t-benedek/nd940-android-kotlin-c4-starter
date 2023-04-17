127b4610e539d9c674d20ed34f40f94a7ed25f15
t-benedek <t.benedek@web.de>
2023-04-17 21:42:25 +0200

    geofence prepared

---
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/geofence/GeoFenceUtils.kt
@@ -0,0 +1,27 @@
+package com.udacity.project4.locationreminders.geofence
+
+import android.content.Context
+import com.google.android.gms.location.GeofenceStatusCodes
+import com.udacity.project4.R
+
+internal object GeofenceConstants{
+    const val GEOFENCE_RADIUS_IN_METERS = 100f
+    const val ACTION_GEOFENCE_EVENT =
+        "ACTION_GEOFENCE_EVENT"
+}
+
+fun errorMessage(context: Context, errorCode: Int): String {
+    val resources = context.resources
+    return when (errorCode) {
+        GeofenceStatusCodes.GEOFENCE_NOT_AVAILABLE -> resources.getString(
+            R.string.geofence_not_available
+        )
+        GeofenceStatusCodes.GEOFENCE_TOO_MANY_GEOFENCES -> resources.getString(
+            R.string.geofence_too_many_geofences
+        )
+        GeofenceStatusCodes.GEOFENCE_TOO_MANY_PENDING_INTENTS -> resources.getString(
+            R.string.geofence_too_many_pending_intents
+        )
+        else -> resources.getString(R.string.geofence_unknown_error)
+    }
+}
--- starter/app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
@@ -2,12 +2,14 @@ package com.udacity.project4.locationreminders.geofence
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.core.app.JobIntentService
 import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingEvent
+import com.udacity.project4.R
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
-import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.project4.utils.sendNotification
 import kotlinx.coroutines.*
@@ -22,8 +24,8 @@ class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
 
     companion object {
         private const val JOB_ID = 573
+        private const val TAG = "GeofenceTransitionJob"
 
-        //        TODO: call this to start the JobIntentService to handle the geofencing transition events
         fun enqueueWork(context: Context, intent: Intent) {
             enqueueWork(
                 context,
@@ -34,18 +36,25 @@ class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
     }
 
     override fun onHandleWork(intent: Intent) {
-        //TODO: handle the geofencing transition events and
-        // send a notification to the user when he enters the geofence area
-        //TODO call @sendNotification
+
+        val geofencingEvent = GeofencingEvent.fromIntent(intent)
+
+        if (geofencingEvent.hasError()) {
+            val errorMessage = errorMessage(applicationContext, geofencingEvent.errorCode)
+            Log.e(TAG, errorMessage)
+            return
+        }
+
+        if (geofencingEvent.geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            Log.v(TAG, applicationContext.getString(R.string.geofence_entered))
+            sendNotification(geofencingEvent.triggeringGeofences)
+        }
     }
 
-    //TODO: get the request id of the current geofence
     private fun sendNotification(triggeringGeofences: List<Geofence>) {
-        val requestId = ""
-
-        //Get the local repository instance
+        val requestId = triggeringGeofences[0].requestId
         val remindersLocalRepository: ReminderDataSource by inject()
-//        Interaction to the repository has to be through a coroutine scope
+
         CoroutineScope(coroutineContext).launch(SupervisorJob()) {
             //get the reminder with the request id
             val result = remindersLocalRepository.getReminder(requestId)
